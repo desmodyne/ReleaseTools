c3e6990460ee7ca69d85387a2b3eacd9cc583116
Stefan Schablowski <stefan.schablowski@desmodyne.com>
2016-06-10 16:26:00 +0200

    [#8]: remove dummy snippets

--- HomebrewFormula/dd-release.rb
+++ HomebrewFormula/dd-release.rb
@@ -8,7 +8,6 @@
 #
 # Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class TtRelease < Formula
   desc "DesmoDyne ReleaseTools for automating the release process."
@@ -23,22 +22,7 @@ class TtRelease < Formula
   depends_on "bash"
   
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "cmake", "./build/", *std_cmake_args
     system "make", "install"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test release`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
